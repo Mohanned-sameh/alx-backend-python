d0a4470830c565b124e88b6b001e8a842abf865b
mohanned-sameh <mohanned2678@gmail.com>
2024-05-06 22:33:13 +0300

    chore: Refactor concurrent coroutines code

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Concurrent coroutines"""
-wait_random = __import__("0-basic_async_syntax").wait_random
-import asyncio
+"""Execute multiple coroutines at the same time with async"""
 from typing import List
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    spawn wait_random n times with the specified max_delay.
-    """
-    delays = [wait_random(max_delay) for _ in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)]
+    """Spawns wait_random n times with the specified max_delay
+    and returns the list of all the delays (float values)."""
+    futures = [wait_random(max_delay) for _ in range(n)]
+    futures = asyncio.as_completed(futures)
+    delays = [await future for future in futures]
+    return delays
