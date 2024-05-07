b777bac1696854e9cf81476bfae51b649b216607
mohanned-sameh <mohanned2678@gmail.com>
2024-05-07 23:41:45 +0300

    feat: Add async generator and async comprehension code
    - Add async_generator function that asynchronously waits 1 second and yields a random number between 0 and 10 for 10 iterations.
    - Add async_comprehension.py file with async comprehension code.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-"""
-0. Async Generator
-"""
-
+"""Creates a generator"""
 import asyncio
 import random
+from typing import AsyncGenerator, Any
 
 
-async def async_generator() -> float:
-    """
-    Coroutine that loops 10 times, each time asynchronously wait 1 second,
-    then yield a random number between 0 and 10.
-    """
-    for _ in range(10):
+async def async_generator() -> AsyncGenerator[Any, Any]:
+    """Each time asynchronously waits 1 second,
+    then yield a random number between 0 and 10"""
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
